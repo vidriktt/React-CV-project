110d239f86e2ccb49e070ff0e9a9d2d58204f7cf
Vidrik Toom Tabas <vidriktoom@gmail.com>
2023-04-10 01:43:48 +0300

    CV project dynamic state array functionality start

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import './styles/App.css';
 import Forms from "./components/Forms";
 import Output from "./components/Output";
+import uniqid from "uniqid";
 
 class App extends Component {
     constructor(props) {
@@ -15,10 +16,27 @@ class App extends Component {
             location: "London, UK",
             about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 
-            schoolName: "University of London",
-            schoolTitle: "Informatics",
-            schoolFrom: "2018",
-            schoolTo: "2021",
+            educationList: [{
+                schoolID: uniqid(),
+                schoolName: "University of London",
+                schoolTitle: "Informatics",
+                schoolFrom: "2018",
+                schoolTo: "2021",
+            },
+            {
+                schoolID: uniqid(),
+                schoolName: "University of London",
+                schoolTitle: "Informatics",
+                schoolFrom: "2018",
+                schoolTo: "2021",
+            }],
+            educationListItem: {
+                schoolID: uniqid(),
+                schoolName: "",
+                schoolTitle: "",
+                schoolFrom: "",
+                schoolTo: "",
+            },
 
             workName: "Microsoft",
             workTitle: "Web Developer",
@@ -28,6 +46,10 @@ class App extends Component {
         }
 
         this.onChangeGeneral = this.onChangeGeneral.bind(this);
+        this.onChangeEducation = this.onChangeEducation.bind(this);
+        this.onChangeEducationAdd = this.onChangeEducationAdd.bind(this);
+        this.onClickRemove = this.onClickRemove.bind(this);
+        this.onClickAdd = this.onClickAdd.bind(this);
     }
 
     onChangeGeneral(target) {
@@ -38,6 +60,45 @@ class App extends Component {
         } else {
             this.setState({ [targetName]: target.textContent });
         }
+    }
+
+    onChangeEducation(target) {
+        let targetArr = target.id.split("-");
+
+        let newEducationList = this.state.educationList.map(item => {
+            if (item.schoolID.replace(/\s+/g, "") === targetArr[1].replace(/\s+/g, "")) {
+                console.log(...item);
+                this.setState({
+                    [item]: { ...[item], [targetArr[0]]: target.value }
+                })
+            }
+            console.log(item);
+            return item;
+        })
+
+        this.setState({ educationList: newEducationList });
+    }
+
+    onChangeEducationAdd(target) {
+        let targetName = target.id.split("-")[1];
+        this.setState({ educationListItem: { ...this.state.educationListItem, [targetName]: target.value } });
+    }
+
+    onClickRemove(target) {
+
+    }
+
+    onClickAdd(target) {
+        this.setState({
+            educationList: [...this.state.educationList, this.state.educationListItem],
+            educationListItem: {
+                schoolID: uniqid(),
+                schoolName: "",
+                schoolTitle: "",
+                schoolFrom: "",
+                schoolTo: "",
+            },
+        });
     }
 
     render() {
--- cv-project/src/components/Forms.js
+++ cv-project/src/components/Forms.js
@@ -4,29 +4,58 @@ import '../styles/Forms.css';
 class Forms extends Component {
     render() {
         const {
-            infoName, infoTitle, email, phone, location, about, schoolName, schoolTitle, schoolFrom, schoolTo, workName, workTitle, workFrom, workTo, workAbout
+            infoName, infoTitle, email, phone, location, about, educationList, workName, workTitle, workFrom, workTo, workAbout
         } = this.props.state;
 
-        const { onChangeGeneral } = this.props;
+        const { onChangeGeneral, onChangeEducation, onChangeEducationAdd, onClickRemove, onClickAdd } = this.props;
 
         return (
             <div id="form">
-                <section>
+                <section id="general-info-section">
                     <h3>General info</h3>
-                    <input id="form-infoName" type="text" value={infoName} onChange={e => onChangeGeneral(e.target)}></input>
-                    <input id="form-infoTitle" type="text" value={infoTitle} onChange={e => onChangeGeneral(e.target)}></input>
-                    <input id="form-email" type="text" value={email} onChange={e => onChangeGeneral(e.target)}></input>
-                    <input id="form-phone" type="text" value={phone} onChange={e => onChangeGeneral(e.target)}></input>
-                    <input id="form-location" type="text" value={location} onChange={e => onChangeGeneral(e.target)}></input>
-                    <span id="form-about" contenteditable="true" onInput={e => onChangeGeneral(e.target)}>{about}</span>
+
+                    <input id="form-infoName" type="text" placeholder="Name" value={infoName} onChange={e => onChangeGeneral(e.target)}></input>
+                    <input id="form-infoTitle" type="text" placeholder="Title" value={infoTitle} onChange={e => onChangeGeneral(e.target)}></input>
+                    <input id="form-email" type="text" placeholder="Email" value={email} onChange={e => onChangeGeneral(e.target)}></input>
+                    <input id="form-phone" type="text" placeholder="Phone number" value={phone} onChange={e => onChangeGeneral(e.target)}></input>
+                    <input id="form-location" type="text" placeholder="Location" value={location} onChange={e => onChangeGeneral(e.target)}></input>
+                    <span id="form-about" contentEditable="true" suppressContentEditableWarning={true} onInput={e => onChangeGeneral(e.target)}>{about}</span>
                 </section>
 
                 <section>
                     <h3>Education</h3>
-                    <input id="form-schoolName" type="text" value={schoolName}></input>
-                    <input id="form-schoolTitle" type="text" value={schoolTitle}></input>
-                    <input id="form-schoolFrom" type="text" value={schoolFrom}></input>
-                    <input id="form-schoolTo" type="text" value={schoolTo}></input>
+
+                    {educationList.map((item) => {
+                        return <div key={item.schoolID} className="education-div">
+                            <input id={`schoolName- ${item.schoolID}`} type="text" placeholder="Institution" value={item.schoolName} onChange={e => onChangeEducation(e.target)}></input>
+                            <input id="form-schoolTitle" type="text" placeholder="Title" value={item.schoolTitle} onChange={e => onChangeEducation(e.target)}></input>
+                            <input id="form-schoolFrom" type="text" placeholder="From" value={item.schoolFrom} onChange={e => onChangeEducation(e.target)}></input>
+                            <input id="form-schoolTo" type="text" placeholder="To" value={item.schoolTo} onChange={e => onChangeEducation(e.target)}></input>
+
+                            <button onClick={e => onClickRemove(e.target)}>Remove</button>
+                        </div>
+                    })}
+
+                    <div className="education-div">
+                        <input id="form-schoolName" type="text" placeholder="Institution" onChange={e => onChangeEducationAdd(e.target)}></input>
+                        <input id="form-schoolTitle" type="text" placeholder="Title" onChange={e => onChangeEducationAdd(e.target)}></input>
+                        <input id="form-schoolFrom" type="text" placeholder="From" onChange={e => onChangeEducationAdd(e.target)}></input>
+                        <input id="form-schoolTo" type="text" placeholder="To" onChange={e => onChangeEducationAdd(e.target)}></input>
+
+                        <button onClick={e => onClickAdd(e.target)}>Add</button>
+                    </div>
+
+                    {/* <div>
+                        <input id="form-schoolName" type="text" value={schoolName} onChange={e => onChangeEducation(e.target)}></input>
+                        <input id="form-schoolTitle" type="text" value={schoolTitle} onChange={e => onChangeEducation(e.target)}></input>
+                        <input id="form-schoolFrom" type="text" value={schoolFrom} onChange={e => onChangeEducation(e.target)}></input>
+                        <input id="form-schoolTo" type="text" value={schoolTo} onChange={e => onChangeEducation(e.target)}></input>
+
+                        <div className="buttons-div">
+                            <button onClick={e => onClickRemove(e.target)}>Remove</button>
+                            <button onClick={e => onClickAdd(e.target)}>Add</button>
+                        </div>
+                    </div> */}
                 </section>
 
                 <section>
@@ -35,7 +64,7 @@ class Forms extends Component {
                     <input id="form-workTitle" type="text" value={workTitle}></input>
                     <input id="form-workFrom" type="text" value={workFrom}></input>
                     <input id="form-workTo" type="text" value={workTo}></input>
-                    <span id="form-workAbout" contenteditable="true" onInput={e => onChangeGeneral(e.target)}>{workAbout}</span>
+                    <span id="form-workAbout" contentEditable="true" suppressContentEditableWarning={true} onInput={e => onChangeGeneral(e.target)}>{workAbout}</span>
                 </section>
             </div>
         );
